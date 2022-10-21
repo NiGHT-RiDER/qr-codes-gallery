23db457ca4e248ae3b7639c9e8f8aa23e5b8b495
NiGHT-RiDER <bogdanovicstefan.sb@gmail.com>
2022-10-21 16:36:18 +0200

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,14 +1,14 @@
 const table = [
-    { imgSrc: '/images/qr-codes/11.png', name: "Desk 11" },
-    { imgSrc: '/images/qr-codes/12.png', name: "Desk 12" },
-    { imgSrc: '/images/qr-codes/13.png', name: "Desk 13" },
-    { imgSrc: '/images/qr-codes/14.png', name: "Desk 14" },
-    { imgSrc: '/images/qr-codes/15.png', name: "Desk 15" },
-    { imgSrc: '/images/qr-codes/16.png', name: "Desk 16" },
-    { imgSrc: '/images/qr-codes/17.png', name: "Desk 17" },
-    { imgSrc: '/images/qr-codes/18.png', name: "Desk 18" },
-    { imgSrc: '/images/qr-codes/19.png', name: "Desk 19" },
-    { imgSrc: '/images/qr-codes/20.png', name: "Desk 20" },
+    { imgSrc: '/images/qr-codes/11.png', name: "Desk 11" , link : "https://m.commuty.net/organisations/O-49rRW9/resources/68b5a0fe-edde-44ec-8999-9f50b1d49f42"},
+    { imgSrc: '/images/qr-codes/12.png', name: "Desk 12" , link : "https://m.commuty.net/organisations/O-49rRW9/resources/4e69a58d-265d-472c-92fb-8222e8c54830"},
+    { imgSrc: '/images/qr-codes/13.png', name: "Desk 13" , link : "https://m.commuty.net/organisations/O-49rRW9/resources/f9d437c1-a29f-42cc-a660-f5c9024bd481"},
+    { imgSrc: '/images/qr-codes/14.png', name: "Desk 14" , link : "https://m.commuty.net/organisations/O-49rRW9/resources/091d02d9-dcb4-4e48-83ac-6343a571cd26"},
+    { imgSrc: '/images/qr-codes/15.png', name: "Desk 15" , link : "https://m.commuty.net/organisations/O-49rRW9/resources/460cdfea-9e5a-4ff6-af8b-95ef68a25cdd"},
+    { imgSrc: '/images/qr-codes/16.png', name: "Desk 16" , link : "https://m.commuty.net/organisations/O-49rRW9/resources/ead05746-4ecb-4979-8739-e1c40714f00a"},
+    { imgSrc: '/images/qr-codes/17.png', name: "Desk 17" , link : "https://m.commuty.net/organisations/O-49rRW9/resources/9d343810-836f-48cf-8379-7c07ecb7e4d0"},
+    { imgSrc: '/images/qr-codes/18.png', name: "Desk 18" , link : "https://m.commuty.net/organisations/O-49rRW9/resources/5cca045b-e81e-4032-b05d-73809994b0a1"},
+    { imgSrc: '/images/qr-codes/19.png', name: "Desk 19" , link : "https://m.commuty.net/organisations/O-49rRW9/resources/1856e31a-9cea-4036-9ba3-0c8f65b7360d"},
+    { imgSrc: '/images/qr-codes/20.png', name: "Desk 20" , link : "https://m.commuty.net/organisations/O-49rRW9/resources/45b30bfb-172a-4c97-b1a4-d3bdc46a69fa"},
 ]
 
 export default table
